41ebfd5db55b45920c54eb2d7dc33b1b33f9ab72
keshu23 <kjha3185@gmail.com>
2024-08-22 11:52:40 +0530

    adde favicon and changed the title

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 
 import { Routes, Route } from "react-router-dom";
 
-const Home = lazy(() => import("./components/Home" /));
+const Home = lazy(() => import("./components/Home"));
 
 const Product = lazy(() => import("./components/Products"));
 const Cart = lazy(() => import("./components/Cart"));
